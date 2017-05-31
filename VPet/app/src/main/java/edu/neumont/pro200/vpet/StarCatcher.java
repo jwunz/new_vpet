2327bbc0026d6ac10cdcb543b99c0fe7bd1cce84
Ryan Spargo <ryanspargo@gmail.com>
2017-05-31 10:48:38 -0600

    Created Battle and StarCatcher activities and StarCatcher start screen

---
+++ VPet/app/src/main/java/edu/neumont/pro200/vpet/StarCatcher.java
@@ -0,0 +1,13 @@
+package edu.neumont.pro200.vpet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StarCatcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_star_catcher);
+    }
+}
